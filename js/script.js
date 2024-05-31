bba7f8939a2269e0cb0fa26309894cac218a0dac
darklowlyx <dark.codx.lowly@gmail.com>
2024-05-31 21:35:16 +0500

    version 0.5
    Default version from the local directory

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const reseatBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function () {
+	count++;
+	countLabel.textContent = count;
+
+}
+
+decreaseBtn.onclick = function () {
+	count--;
+	countLabel.textContent = count;
+}
+
+reseatBtn.onclick = function () {
+	count = 0;
+	countLabel.textContent = count;
+}
